abc38d0f1f412947dc0b1fe7a78ae097cf8edea4
Kerry Kidd <kerry@raspikidd.com>
2025-01-08 16:55:47 +0000

    Updated blog colours

--- pages/[slug].vue
+++ pages/[slug].vue
@@ -8,7 +8,7 @@
           <svg class="absolute top-12 left-full transform translate-x-32" width="404" height="384" fill="none" viewBox="0 0 404 384">
             <defs>
               <pattern id="74b3fd99-0a6f-4271-bef2-e80eeafdf357" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
-                <rect x="0" y="0" width="4" height="4" class="text-blue-300" fill="currentColor" />
+                <rect x="0" y="0" width="4" height="4" class="text-red-300" fill="currentColor" />
               </pattern>
             </defs>
             <rect width="404" height="384" fill="url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)" />
@@ -16,7 +16,7 @@
           <svg class="absolute top-1/2 right-full transform -translate-y-1/2 -translate-x-32" width="404" height="384" fill="none" viewBox="0 0 404 384">
             <defs>
               <pattern id="f210dbf6-a58d-4871-961e-36d5016a0f49" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
-                <rect x="0" y="0" width="4" height="4" class="text-blue-300" fill="currentColor" />
+                <rect x="0" y="0" width="4" height="4" class="text-red-300" fill="currentColor" />
               </pattern>
             </defs>
             <rect width="404" height="384" fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)" />
@@ -24,7 +24,7 @@
           <svg class="absolute bottom-12 left-full transform translate-x-32" width="404" height="384" fill="none" viewBox="0 0 404 384">
             <defs>
               <pattern id="d3eb07ae-5182-43e6-857d-35c643af9034" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
-                <rect x="0" y="0" width="4" height="4" class="text-blue-300" fill="currentColor" />
+                <rect x="0" y="0" width="4" height="4" class="text-red-300" fill="currentColor" />
               </pattern>
             </defs>
             <rect width="404" height="384" fill="url(#d3eb07ae-5182-43e6-857d-35c643af9034)" />
@@ -34,10 +34,10 @@
       <div class="relative px-4 sm:px-6 lg:px-8">
         <div class="text-lg max-w-prose mx-auto" v-if="story">
           <h1>
-            <span class="block text-base text-center text-blue-900 font-semibold tracking-wide uppercase">
+            <span class="block text-base text-center text-red-900 font-semibold tracking-wide uppercase">
               {{ story.content.author.content.Name }}
             </span>
-            <span class="mt-2 block text-4xl text-center leading-8 font-extrabold tracking-tight text-blue-950 sm:text-4xl">
+            <span class="mt-2 block text-4xl text-center leading-8 font-extrabold tracking-tight text-grey-800 sm:text-4xl">
               {{ story.content.title }}
             </span>
           </h1>
