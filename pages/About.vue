574b91ae99a0f79e469722af295a345f4c48e349
Kerry Kidd <kerry@raspikidd.com>
2025-01-09 14:26:27 +0000

    Updated About page with CTA

--- pages/About.vue
+++ pages/About.vue
@@ -4,11 +4,8 @@
     <main class="lg:relative">
       <div class="mx-auto max-w-7xl w-full pt-16 pb-20 text-center lg:py-30 lg:text-center">
         <div class="px-4 lg:w-1/2 sm:px-8 xl:pr-16">
-          <!-- <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
-            <span class="block xl:inline" style = "color:#A42530"> About </span>
-          </h1> -->
-          <h2 class=" pt-8 text-2xl tracking-tight font-extrabold text-gray-900 sm:text-3xl md:text-4xl lg:text-3xl xl:text-4xl">
-            <span class="block xl:inline" style="color:#0E7452"> 
+          <h2 class=" pt-8 text-2xl tracking-tight font-extrabold text-emerald-800 sm:text-3xl md:text-4xl lg:text-3xl xl:text-4xl">
+            <span class="block xl:inline"> 
               Imagine being able to program a computer that you can pick up and put in your pocket
             </span>
           </h2>
@@ -66,20 +63,26 @@
         <img class="absolute inset-0 w-full h-full object-cover" src="/public/images/me.jpeg" alt="Image of RaspiKidd programming">
       </div>
     </main>
+    <div class="relative isolate overflow-hidden px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0" style="background-color:#A1C4B5">
+      <div class="text-center justify-center lg:mx-0 lg:flex-auto lg:py-10">
+        <h2 class="text-emerald-800 text-3xl font-bold tracking-tight sm:text-4xl">
+          Sign Up to our Newsletter
+        </h2>
+        <NewsletterForm />
+        <h2 class="text-emerald-800 text-3xl font-bold tracking-tight sm:text-4xl">
+          Book a Call
+        </h2>
+        <div class="flex-shrink-0 lg:py-5">
+          <a href="https://app.simplymeet.me/raspikidd/discovery" target="_blank" class="relative inline-flex items-center gap-x-1.5 rounded-md bg-red-700 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#22c55e" class="size-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
+            </svg>
+            Book Now
+          </a>
+        </div>
+      </div>
+    </div>
     <Footer />
   </div>
 </template>
-
-<!--<script lang="ts">
-  import Vue from "vue";
-  import Header from "~/components/Header.vue";
-  import Footer from "~/components/Footer.vue";
-
-  export default Vue.extend({
-    components: {
-      Header,
-      Footer
-    }
-  });
-</script>-->
 
