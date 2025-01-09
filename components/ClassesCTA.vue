feeff1806776b866092e866640fd2590d564e4ad
Kerry Kidd <kerry@raspikidd.com>
2025-01-09 14:55:04 +0000

    Added Meeting Link

--- components/ClassesCTA.vue
+++ components/ClassesCTA.vue
@@ -14,7 +14,7 @@
             <NuxtLink to="/ComingSoon" class="rounded-md bg-sky-200 px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">
                 Book Classes
             </NuxtLink>
-            <a href="#" class="text-sm/6 font-semibold text-white">
+            <a href="https://app.simplymeet.me/raspikidd/classinfo" class="text-sm/6 font-semibold text-white">
                 Book a Call 
                 <span aria-hidden="true">
                     →
