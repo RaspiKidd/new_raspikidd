40447b514d2c05d3d95005f345b5a58f88b18646
Kerry Kidd <kerry@raspikidd.com>
2025-01-21 17:20:15 +0000

    Updated paragraph text size

--- pages/index.vue
+++ pages/index.vue
@@ -24,7 +24,7 @@
           <h2 class="mt-5 text-pretty text-3xl font-semibold tracking-tight text-emerald-800 sm:text-4xl">
             What We Do
           </h2>
-          <ul role="list" class="mt-8 space-y-8 text-gray-700">
+          <ul role="list" class="mt-8 space-y-8 text-gray-700 text-xl">
             <li class="flex gap-x-3">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#991b1b" class="size-10">
                 <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32l8.4-8.4Z" />
@@ -69,7 +69,7 @@
           <h2 class="mt-5 text-pretty text-3xl font-semibold tracking-tight text-emerald-800 sm:text-4xl">
             Why Choose RaspiKidd?
           </h2>
-          <ul role="list" class="mt-8 space-y-8 text-gray-700 font-semibold">
+          <ul role="list" class="mt-8 space-y-8 text-gray-700 text-xl">
             <li>
               We make STEM affordable for families and educators. 
             </li>
