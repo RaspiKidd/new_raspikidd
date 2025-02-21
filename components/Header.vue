4853874598a74f82904c63dad713b1da3843a512
Kerry Kidd <kerry@raspikidd.com>
2025-02-21 15:29:39 +0000

    removed blog from header

--- components/Header.vue
+++ components/Header.vue
@@ -86,11 +86,11 @@
 
             <!--<a href="https://blog.raspikidd.com/" target="_Blank" class="text-base font-medium text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
               Blog
-            </a>-->
+            </a>
 
            <NuxtLink to="/Blog" class="text-base font-medium text-gray-700 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
               Blog
-            </NuxtLink>
+            </NuxtLink>-->
 
             <NuxtLink to="/HireMe" class="text-base font-medium text-gray-700 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
               Hire Us
@@ -179,11 +179,11 @@
                       
                              <!--<a href="https://blog.raspikidd.com/" target="_blank" class="text-base font-medium text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 p-3">
                               Blog
-                            </a> -->
+                            </a> 
 
                             <NuxtLink to="/Blog" class="text-base font-medium text-gray-700 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 p-3">
                               Blog
-                            </NuxtLink> 
+                            </NuxtLink> -->
 
                             <NuxtLink to="/HireMe" class="text-base font-medium text-gray-700 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 p-3">
                               Hire Us
